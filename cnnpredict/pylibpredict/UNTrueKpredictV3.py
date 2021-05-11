201035d55d0f62af254d5e7b6d518f4595b4b0cc
samir lehaff <samir@lehaff.dk>
2021-05-11 08:43:09 +0200

    sa

--- cnnpredict/pylibpredict/UNTrueKpredictV3.py
+++ cnnpredict/pylibpredict/UNTrueKpredictV3.py
@@ -455,6 +455,7 @@ bfolder = '/home/samir/Desktop/blender/pycode/Ntarget/'
 Lmodel = load_L_model()
 Hmodel = load_H_model()
 
+
 for i in range(len(os.listdir(bfolder))-1):
     # folder = '/home/samir/Desktop/blender/pycode/15trainMan/render'
     # folder = '/home/samir/db3/scan/static/scan_folder/scan_im_folder'
