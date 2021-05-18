af4cede85ffe9a9da97e5f165f0049ed82b3d78f
Samir_Lehaff <samir@lehaff.dk>
2021-05-18 09:44:00 +0200

    sa

--- cnntrain/pylibtools/filtermaps.py
+++ cnntrain/pylibtools/filtermaps.py
@@ -55,8 +55,8 @@ def load_L_model():
 
 
 # load the model
-# model = VGG16()
-model = load_H_model()
+model = VGG16()
+# model = load_H_model()
 # retrieve weights from the second hidden layer
 filters, biases = model.layers[1].get_weights()
 # normalize filter values to 0-1 so we can visualize them
@@ -78,37 +78,5 @@ for i in range(n_filters):
 		ix += 1
 # show the figure
 pyplot.show()
-
-# load the model
-model = VGG16()
-# redefine model to output right after the first hidden layer
-model = Model(inputs=model.inputs, outputs=model.layers[1].output)
-model.summary()
-# load the image with the required shape
-mybird ='/home/samir/dblive/cnntrain/pylibtools/bird.jpg'
-img = load_img(mybird, target_size=(224, 224))
-# convert the image to an array
-img = img_to_array(img)
-# expand dimensions so that it represents a single 'sample'
-img = expand_dims(img, axis=0)
-# prepare the image (e.g. scale pixel values for the vgg)
-img = preprocess_input(img)
-# get feature map for first hidden layer
-feature_maps = model.predict(img)
-# plot all 64 maps in an 8x8 squares
-square = 8
-ix = 1
-for _ in range(square):
-	for _ in range(square):
-		# specify subplot and turn of axis
-		ax = pyplot.subplot(square, square, ix)
-		ax.set_xticks([])
-		ax.set_yticks([])
-		# plot filter channel in grayscale
-		pyplot.imshow(feature_maps[0, :, :, ix-1], cmap='gray')
-		ix += 1
-# show the figure
-pyplot.show()
-
 
 
