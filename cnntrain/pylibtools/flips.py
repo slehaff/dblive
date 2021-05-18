027c12e34afaf12f647c160f25b0ec8a057317ce
samir lehaff <samir@lehaff.dk>
2021-05-18 10:58:21 +0200

    Merge branch 'master' of https://github.com/slehaff/dblive

--- cnntrain/pylibtools/flips.py
+++ cnntrain/pylibtools/flips.py
@@ -5,19 +5,16 @@ import os
 import PIL
 from PIL import Image, ImageOps
 
-inputFolder = '/home/samir/Desktop/blender/pycode/15trainMat/'
-oufolder = '/home/samir/Desktop/blender/pycode/15trainMat/'
+inputFolder = '/home/samir/Desktop/blender/pycode/15may21/batch/'
 IMAGECOUNT = len(os.listdir(inputFolder))
 for i in range(IMAGECOUNT):
     folder = inputFolder + 'render'+str(i)+'/'
-    os.mkdir('/home/samir/Desktop/blender/pycode/15trainMat/'+'render'+str(i+IMAGECOUNT))
+    os.mkdir(inputFolder+'render'+str(i+IMAGECOUNT))
+    os.mkdir(inputFolder+'render'+str(i+2*IMAGECOUNT))
     for j in range(10):
-        im = Image.open(folder+'image'+str(j)+'.png')
-        mirror = ImageOps.mirror(im)
-        # image= cv2.imread(folder+'image'+str(j)+'.png')
-        # flipped = cv2.flip(image,1)
-        
-        mirror.save('/home/samir/Desktop/blender/pycode/15trainMat/'+'render'+str(i+IMAGECOUNT)+'/' +'image'+str(j)+'.png')
-        
-        
-        print('/home/samir/Desktop/blender/pycode/15trainMat/'+'render'+str(i+IMAGECOUNT)+'/' +'image'+str(j)+'.png')
+        image= cv2.imread(folder+'image'+str(j)+'.png')
+        fliph = cv2.flip(image,1)
+        flipv = cv2.flip(image,0)
+        cv2.imwrite(inputFolder+'render'+str(i+IMAGECOUNT)+'/' +'image'+str(j)+'.png', fliph)
+        cv2.imwrite(inputFolder+'render'+str(i+2*IMAGECOUNT)+'/' +'image'+str(j)+'.png', flipv)
+        print(inputFolder+'render'+str(i+IMAGECOUNT)+'/' +'image'+str(j)+'.png')
